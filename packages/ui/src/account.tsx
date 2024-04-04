e961e7c754abcfc7e04303d74cfe58ec57efa13f
rabra <rabira.hierpa@gmail.com>
2024-04-04 17:11:10 +0300

    Add Account and List components

---
+++ packages/ui/src/account.tsx
@@ -0,0 +1,8 @@
+export const Account = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>Account Component</h1>
+      {children}
+    </div>
+  );
+};
---
+++ packages/ui/src/list.tsx
@@ -0,0 +1,8 @@
+export const List = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>List Component</h1>
+      {children}
+    </div>
+  );
+};
